f71121ef010ff3c7949488e38170d38f04040b87
c0b4lt-student <84628259+c0b4lt-student@users.noreply.github.com>
2023-04-17 16:18:29 +0200

    Update Department.java

--- src/main/java/com/course/spring_basic/model/Department.java
+++ src/main/java/com/course/spring_basic/model/Department.java
@@ -1,5 +1,6 @@
 package com.course.spring_basic.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class Department {
     private String name;
     @Column(length = 8)
     private String code;
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name="region_id")
     private Region region;
